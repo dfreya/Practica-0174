0117f6958073d22a2227ae91b0e176c9709b0443
dfreya <dbrja.dam@gmail.com>
2017-02-09 09:59:35 +0100

    Creados los Parametros, Iniciación de los Metodos

---
+++ Comida.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class Comida here.
+ * 
+ * @author (Nero Hornet) 
+ * @version (a version number or a date)
+ */
+public class Comida
+{
+    // indicar el nombre de la comida.
+    private String nombreComida;
+    //indicar la cantidad de calorias asociadas a la comida.
+    private int calorias;
+
+    /**
+     * Constructor for objects of class Comida
+     */
+    public Comida()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
---
+++ Persona.java
@@ -0,0 +1,41 @@
+
+/**
+ * Write a description of class Persona here.
+ * 
+ * @author (Nero Hornet) 
+ * @version (a version number or a date)
+ */
+public class Persona
+{
+    // guerda el nombre de la persona.
+    private String nombrePersona;
+    // guerda si la persona es hobmbre o mujer.
+    private boolean hombreOMujer;
+    // guerda el peso de la persona.
+    private int peso;
+    // guerda la altura de la persona.
+    private int altura;
+    // guerda los años de la persona.
+    private int year;
+
+    /**
+     * Constructor for objects of class Persona
+     */
+    public Persona()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
